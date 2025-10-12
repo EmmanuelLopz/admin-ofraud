e63c238d4f85edc9e5efb383bb72b36395025603
David-Brnb <321171197@ciencias.unam.mx>
2025-10-12 10:29:23 -0600

    add: card component for the other components

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import Card from "@/src/components/Card";
 import Sidebar from "../../components/Sidebar";
 
 export default function Dashboard() {
@@ -9,6 +10,9 @@ export default function Dashboard() {
       {/* Contenido principal */}
       <main className="ml-[300px] flex-1 bg-[#f6f7fb] p-10">
         <h1 className="text-3xl font-semibold text-[#060025]">Dashboard</h1>
+        <Card className="p-2 text-sm w-1/2">
+          <p>This is a small card</p>
+        </Card>
       </main>
     </div>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,27 @@
+// src/components/Card.tsx
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
+
+type CardProps = {
+  children: ReactNode;
+  className?: string;
+
+} & React.ComponentProps<'div'>;
+
+function Card({ children, className, ...props }: CardProps) {
+  const combinedClasses = clsx(
+    'bg-white',
+    'rounded-lg',
+    'p-6',
+    'shadow-[0_0_15px_rgba(0,0,0,0.1)]',
+    className
+  );
+
+  return (
+    <div className={combinedClasses} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export default Card;

c0a0662113ee8740960f109f244d90162cb86a24
David-Brnb <321171197@ciencias.unam.mx>
2025-10-14 12:27:43 -0600

    add: tools page and sidebar creation

---
+++ src/app/tools/Tools.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Sidebar from "@/src/components/Sidebar";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Tools() {
+  const router = useRouter();
+  const [hoveredIndex, setHoveredIndex] = useState<Number|null>(null);
+
+  return (
+        <div className="flex h-screen overflow-hidden">
+            {/* Sidebar a la izquierda */}
+            
+            <div className="w-1/6">
+                <Sidebar />
+            </div>
+
+            {/* Contenido principal */}
+            <div className="flex-1 flex flex-col bg-[#f6f7fb]">
+                {/* Header fijo */}
+                <div className="p-10">
+                    <h1 className="text-5xl font-semibold text-[#060025]">Herramientas</h1>
+                </div>
+            </div>
+        </div>
+    );
+}

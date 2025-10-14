b8b1f96e1f149d102ccc9d67a96b128b891aad7f
David-Brnb <321171197@ciencias.unam.mx>
2025-10-13 20:12:50 -0600

    add: implementations of accepts reports page

--- src/app/validation/Validation.tsx
+++ src/app/validation/Validation.tsx
@@ -1,17 +1,64 @@
+'use client';
+import CustomButton from "@/src/components/CustomButton";
 import Sidebar from "../../components/Sidebar";
+import ReportCard from "@/src/components/report/ReportCard";
+import reportes from "@/src/types/reportExamples";
+import { useState } from "react";
 
 export default function Validation() {
+  const [reports, setReports] = useState(reportes);
+
+  // Función para eliminar un reporte por índice o id
+  function handleRemove(indexToRemove: number) {
+    setReports((prevReports) => prevReports.filter((_, i) => i !== indexToRemove));
+  }
+
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-screen overflow-hidden">
       {/* Sidebar a la izquierda */}
       <div className="w-1/6">
         <Sidebar />
       </div> 
 
       {/* Contenido principal */}
-      <main className="flex-1 bg-[#f6f7fb] p-10">
-        <h1 className="text-3xl font-semibold text-[#060025]">Aceptación de reportes</h1>
-      </main>
+      {/* Contenido principal */}
+      <div className="flex-1 flex flex-col bg-[#f6f7fb]">
+        {/* Header fijo */}
+        <div className="p-10">
+          <h1 className="text-5xl font-semibold text-[#060025]">Reportes</h1>
+        </div>
+
+        {/* Contenido con scroll */}
+        <div className="flex-1 overflow-y-auto px-10 p-5">
+          <div className="flex flex-wrap justify-center gap-x-24 gap-y-10">
+            {reports.map((reporte, i) => (
+              <ReportCard 
+                key={i} 
+                reporte={reporte} 
+                className="
+                  hover:scale-105 
+                  transition-transform
+                "
+              >
+
+                <div className="flex flex-row gap-2 pt-8">
+                  <CustomButton 
+                    label="Accept" 
+                    className="bg-green-500 hover:bg-green-600 w-1/2"
+                    onClick={()=>handleRemove(i)}
+                  />
+                  <CustomButton 
+                    label="Reject" 
+                    className="bg-red-500 hover:bg-red-600 w-1/2"
+                    onClick={()=>handleRemove(i)}
+                  />
+                </div>
+                
+              </ReportCard>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import clsx from 'clsx';
+
+type CustomButtonProps = {
+  label: string;
+  onClick?: () => void;
+  className?: string;
+} & React.ComponentProps<'button'>;
+
+function CustomButton({ label, onClick, className, ...props }: CustomButtonProps) {
+  const combinedClasses = clsx(
+    'text-white',
+    'py-2',
+    'px-4', 
+    'rounded-lg', 
+    'active:scale-95',
+    'transition', 
+    'duration-200',
+    'shadow-[0_0_15px_rgba(0,0,0,0.1)]',
+    className
+  );
+
+  return (
+    <button className={combinedClasses} onClick={onClick} {...props}>
+      {label}
+    </button>
+  );
+}
+
+export default CustomButton;
+

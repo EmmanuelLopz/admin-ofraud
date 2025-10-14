04d3b865096df97dee5b8f96689f6c0962b51641
David-Brnb <321171197@ciencias.unam.mx>
2025-10-14 13:36:46 -0600

    erase the icons

--- src/app/tools/Tools.tsx
+++ src/app/tools/Tools.tsx
@@ -3,10 +3,13 @@
 import Sidebar from "@/src/components/Sidebar";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import * as Icons from "lucide-react";
 
 export default function Tools() {
   const router = useRouter();
   const [hoveredIndex, setHoveredIndex] = useState<Number|null>(null);
+  const iconNameFromDB = "Users"; // or "Tool", "FileText", etc.
+  const LucideIcon = Icons[iconNameFromDB];
 
   return (
         <div className="flex h-screen overflow-hidden">
@@ -22,7 +25,14 @@ export default function Tools() {
                 <div className="p-10">
                     <h1 className="text-5xl font-semibold text-[#060025]">Herramientas</h1>
                 </div>
-                <img src="/public/glyphs/0.circle.fill.png" alt="" />
+
+                <img src="/symbols/0.circle.fill.svg" alt="coming soon" className="w-1/6 h-auto text-red-300" />
+
+                {LucideIcon ? <LucideIcon size={24} color="blue" /> : null}
+
+                <div className="grid grid-cols-4 gap-4 p-4">
+                    Hola
+                </div>
             </div>
         </div>
     );
